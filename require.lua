b6b3d1e24dd33e2f5404e807f74965c6ab4cea39
cr1tcal3 <iloveexploiting123@gmail.com>
2024-11-09 16:03:04 +0800

    Update require.lua

--- require.lua
+++ require.lua
@@ -2,7 +2,7 @@ local function crequire(p)
     local s,f = pcall(readfile,p..".lua")
     if not s then return error("Failed: "..p) end
     local e = setmetatable({},{__index = _G})
-    local c,r = load(f,"="..p,"bt",e)
+    local c,r = loadstring(f,"="..p,"bt",e)
     if not c then return error(r) end
     local x = c()
     package.loaded[p] = x or true
